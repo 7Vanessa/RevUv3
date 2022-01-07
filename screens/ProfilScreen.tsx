7ccde488813be08792274fd87f942c3bc1ad0c57
7Vanessa <vmohammedi@outlook.fr>
2022-01-07 16:12:49 +0100

    final

--- screens/ProfilScreen.tsx
+++ screens/ProfilScreen.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {StyleSheet, View, Text, Image, ImageBackground} from "react-native";
 import {auth} from "../firebase";
+import {Container} from "./QuizzScreen";
 
 export default function ProfilPicture() {
+
+    const photo = {uri: auth.currentUser?.photoURL ? auth.currentUser.photoURL : "https://zupimages.net/up/22/01/nu7y.jpeg"}
 
     return(
         <View style={styles.container}>
             <Text style={styles.info}>User Informations</Text>
+            <Text style={styles.general}>Profil Picture:</Text>
+            <View style={styles.container_image}>
+                <ImageBackground source={photo} style={styles.image}/>
+            </View>
             <Text style={styles.general}>Pseudo:</Text>
             <Text style={styles.general_info}>{auth.currentUser?.displayName}</Text>
             <Text style={styles.general}>Mail:</Text>
@@ -18,7 +25,22 @@ export default function ProfilPicture() {
 const styles = StyleSheet.create({
     container: {
         paddingLeft: 15,
-        paddingRight: 15
+        paddingRight: 15,
+        width: '100%',
+        height: '100%'
+    },
+
+    container_image: {
+        height: 150,
+        width: '100%'
+    },
+
+    image: {
+        width: '100%',
+        height: 150,
+        fontSize: 18,
+        marginBottom: 15,
+        alignContent: 'center'
     },
 
     info: {
